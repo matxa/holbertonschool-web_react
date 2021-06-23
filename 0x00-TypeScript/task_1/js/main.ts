f884cc5f86b5db240c736c343133075bd478aff2
matxa <42968545+matxa@users.noreply.github.com>
2021-06-22 23:58:59 -0400

    task1

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -18,3 +18,32 @@ export interface printTeacherFunction {
 export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
 	return `${firstName[0]}. ${lastName}`;
 }
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+export interface StudentClassInterface {
+	readonly firstName: string;
+	readonly lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+export interface StudentConstructor {
+	new(firstName: string, lastName: string): StudentClassInterface;
+}
