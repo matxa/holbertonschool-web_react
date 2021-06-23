e925ef05eec738bd48b0eac1f04eb5dbcb87ed83
matxa <42968545+matxa@users.noreply.github.com>
2021-06-22 21:46:41 -0400

    task1

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -11,7 +11,7 @@ export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
+export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
 
