45b0a4b9e04cdb6a5e42cc2e3ea01d5d30fcf637
Marcelo <42968545+matxa@users.noreply.github.com>
2021-06-24 22:44:08 -0400

    Delete 0x01-Webpack/src directory

--- 0x01-Webpack/src/index.js
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-
-$('body').append("<h1>Holberton Dashboard</h1>")
-$('body').append("<h1>Dashboard data for the students</h1>")
-$('body').append("<h1>Copyright - Holberton School</h1>")
