b0f7768b22d0fe62944e92e714a0976e5cf00ae4
matxa <42968545+matxa@users.noreply.github.com>
2021-06-24 22:41:19 -0400

    task 0

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$('body').append("<h1>Holberton Dashboard</h1>")
+$('body').append("<h1>Dashboard data for the students</h1>")
+$('body').append("<h1>Copyright - Holberton School</h1>")
