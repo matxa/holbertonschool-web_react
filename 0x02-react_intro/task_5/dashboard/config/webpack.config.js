2b27de2d0884b6ada33fc8bb70f034c9987c7a34
matxa <42968545+matxa@users.noreply.github.com>
2021-07-12 19:09:20 -0400

    Q10

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -14,6 +14,36 @@ module.exports = {
   module: {
     rules: [
       {
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/react']
+					}
+				}
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/react']
+					}
+				}
+			},
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
