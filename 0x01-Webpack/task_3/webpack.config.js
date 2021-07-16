5a080a3271140b79eda254546db25abcd6d01b60
matxa <42968545+matxa@users.noreply.github.com>
2021-07-16 17:52:43 -0400

    3

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -19,8 +19,9 @@ module.exports = {
 		filename: '[name].bundle.js'
 	},
 	mode: 'development',
+	devtool: 'inline-source-map',
 	devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: './public',
     compress: true,
     port: 8564,
   },
