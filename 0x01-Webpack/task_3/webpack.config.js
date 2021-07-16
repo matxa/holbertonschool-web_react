3083ebf8ed08bbd372384a8dacf125d4160af219
matxa <42968545+matxa@users.noreply.github.com>
2021-07-16 17:40:25 -0400

    3

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -11,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Task 3',
     }),
+		new CleanWebpackPlugin()
   ],
 	output: {
 		path: path.resolve(__dirname, 'public'),
