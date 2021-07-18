97eb6f9a7b1e6e3664682b16b1076139ca922b24
matxa <42968545+matxa@users.noreply.github.com>
2021-07-17 20:00:29 -0400

    move <Notifications to App.js />

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
 
 export default function App() {
   return (
     <React.Fragment>
       <div className="App">
+        <Notifications />
         <Header />
         <Login />
         <Footer />
