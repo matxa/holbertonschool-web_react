c0d3e91c460723006f4b5ae7d7ec4dec05bf5460
matxa <42968545+matxa@users.noreply.github.com>
2021-07-18 21:14:28 -0400

    2

---
+++ 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotificationItem(props) {
+  return (
+    <li
+      data-notification-type={props.type}
+      dangerouslySetInnerHTML={props.html}
+    >{props.value}</li>
+  );
+}
